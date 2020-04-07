3190e99c54fdeb44b70185bb6b8ae6013963c6e8
Taaha Khan <61217792+taaha-khan@users.noreply.github.com>
2020-04-07 14:05:02 -0700

    Update cell.js

--- cell.js
+++ cell.js
@@ -1,16 +1,23 @@
+
 class Cell {
     constructor(x, y) {
         this.x = x;
         this.y = y;
-
         this.isWall = false;
+        this.neighbors = [];
+
+        // A* Stuff
         this.parent = null;
-        
         this.g = 0;
         this.h = 0;
         this.f = 0;
+
+        // Dijkstra Stuff
+        this.prev = undefined;
+        this.dist = Infinity;
     }
 
+    // Show Functions
     show() {
         fill(51);
         stroke(255);
@@ -19,7 +26,7 @@ class Cell {
     }
 
     highlight() {
-        fill(255, 0, 0);
+        fill(0, 255, 0);
         stroke(255);
         rect(this.x, this.y, scl, scl);
     }
@@ -36,5 +43,32 @@ class Cell {
             stroke(255);
             rect(this.x, this.y, scl, scl);
         }
+    }
+}
+
+
+function neighboring(node) {
+    let neighbors = [];
+
+    for (cell of grid) {
+        if (!cell.isWall) {
+            if (cell.x == node.x && cell.y == node.y + scl) {
+                neighbors.push(cell);  // Below
+            } if (cell.x == node.x && cell.y == node.y - scl) {
+                neighbors.push(cell);  // Above
+            } if (cell.x == node.x + scl && cell.y == node.y) {
+                neighbors.push(cell);  // Onright
+            } if (cell.x == node.x - scl && cell.y == node.y) {
+                neighbors.push(cell);  // Onleft
+            }
+            if (neighbors.length == 4) return neighbors;
+        }
+    }
+    return neighbors;
+}
+
+function getNeighbors(grid) {
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].neighbors = neighboring(grid[i]);
     }
 }
